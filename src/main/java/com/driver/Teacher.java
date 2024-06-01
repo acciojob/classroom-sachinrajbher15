e812d6a3b7d34a883a9e2bb4617ebe42a34b9f1c
SACHIN KUMAR RAJBHER <86919694+sachinrajbher15@users.noreply.github.com>
2024-06-02 00:49:09 +0530

    Update Teacher.java

--- src/main/java/com/driver/Teacher.java
+++ src/main/java/com/driver/Teacher.java
@@ -3,14 +3,12 @@ package com.driver;
 public class Teacher {
 
     private String name;
-
     private int numberOfStudents;
-
     private int age;
-
+    
     public Teacher() {
-
     }
+
     public Teacher(String name, int numberOfStudents, int age) {
         this.name = name;
         this.numberOfStudents = numberOfStudents;
